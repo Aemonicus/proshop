3f49a6cd4aeab9568870aa5fea5db6a138fcdf9b
Florent VICENTE <vicentegroupe@gmail.com>
2022-03-25 16:28:29 +0100

    product detail screen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Row, Col, Image, ListGroupe, Card, Button } from 'react-bootstrap'
+import { Row, Col, Image, ListGroupe, Card, Button, ListGroup } from 'react-bootstrap'
 import Rating from "../components/Rating"
 import products from "../products"
 
@@ -9,6 +9,56 @@ const ProductScreen = ({ match }) => {
   return (
     <>
       <Link className="btn btn-light my-3" to="/">Go Back </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+          </ListGroup>
+          <ListGroup.Item>
+            <Rating value={product.rating} text={`${product.reviews} reviews`} />
+          </ListGroup.Item>
+          <ListGroup.Item>
+            Price: {product.price} €
+          </ListGroup.Item>
+          <ListGroup.Item>
+            Description: {product.description}
+          </ListGroup.Item>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <Row>
+                  <Col>
+                    Price:
+                  </Col>
+                  <Col>
+                    <strong>{product.price}€</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>
+                    Status:
+                  </Col>
+                  <Col>
+                    {product.countInStock > 0 ? "In Stock" : "Not In Stock"}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button className="btn-block" type="button" disabled={product.countInStock === 0}>Add To Cart</Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
     </>
   )
 }
