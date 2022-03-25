4deaea7efb47693c33ef90d64e18e2dfad9a5d5c
Florent VICENTE <vicentegroupe@gmail.com>
2022-03-25 23:12:03 +0100

    mongodb altas & compass & connection to database

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,12 @@
-const express = require("express");
-const cors = require("cors");
-const products = require("./data/products");
+import express from "express";
+import dotenv from "dotenv"
+import connectDB from "./config/db.js"
+import cors from "cors"
+import products from "./data/products.js"
+
+dotenv.config()
+
+connectDB()
 
 const app = express();
 
@@ -19,4 +25,6 @@ app.get("/api/products/:id", (req, res) => {
   res.json(product)
 })
 
-app.listen(5000, console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`));
