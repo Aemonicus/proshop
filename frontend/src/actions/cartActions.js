66cc052718790a7806312ed580ef8a0dc9702fdc
Florent VICENTE <vicentegroupe@gmail.com>
2022-04-16 15:26:35 +0200

    cart add to cart functionnality

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,5 +1,10 @@
-import axios from "axios"
-import { CART_ADD_ITEM } from "../constants/cartConstants"
+import axios from 'axios'
+import {
+  CART_ADD_ITEM,
+  CART_REMOVE_ITEM,
+  CART_SAVE_SHIPPING_ADDRESS,
+  CART_SAVE_PAYMENT_METHOD,
+} from '../constants/cartConstants'
 
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
@@ -8,7 +13,7 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
   dispatch({
     type: CART_ADD_ITEM,
     payload: {
-      product: data_id,
+      product: data._id,
       name: data.name,
       image: data.image,
       price: data.price,
@@ -20,4 +25,31 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
   // getState va tapper dans le state redux global, il faut d'abord taper dans la propriété globale cart puis redescendre
   // Le résultat doit être envoyé sour forme de string car on ne peut stocker que des strings dans le localStorage, d'où l'utilisation de JSON.stringify()
   localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems))
+}
+
+export const removeFromCart = (id) => (dispatch, getState) => {
+  dispatch({
+    type: CART_REMOVE_ITEM,
+    payload: id,
+  })
+
+  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
+}
+
+export const saveShippingAddress = (data) => (dispatch) => {
+  dispatch({
+    type: CART_SAVE_SHIPPING_ADDRESS,
+    payload: data,
+  })
+
+  localStorage.setItem('shippingAddress', JSON.stringify(data))
+}
+
+export const savePaymentMethod = (data) => (dispatch) => {
+  dispatch({
+    type: CART_SAVE_PAYMENT_METHOD,
+    payload: data,
+  })
+
+  localStorage.setItem('paymentMethod', JSON.stringify(data))
 }
