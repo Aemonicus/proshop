c6fb3138638acbf8d3eec35586e864ff8aa2af29
Florent VICENTE <vicentegroupe@gmail.com>
2022-04-24 17:50:33 +0200

    profile screen & get user details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
 import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
+import ProfileScreen from './screens/ProfileScreen'
 import { Container } from "react-bootstrap"
 import { BrowserRouter as Router, Route } from "react-router-dom"
 
@@ -16,6 +17,7 @@ function App() {
         <Container>
           <Route path="/login" component={LoginScreen} />
           <Route path="/register" component={RegisterScreen} />
+          <Route path="/profile" component={ProfileScreen} />
           <Route path="/product/:id" component={ProductScreen} />
           {/* En mettant un ? derrière le paramètre id, j'indique que le paramètre est facultatif. Si le client veut aller dans son shopping cart vide, il n'y aura pas d id donc le id ne doit pas être obligatoire */}
           <Route path="/cart/:id?" component={CartScreen} />
