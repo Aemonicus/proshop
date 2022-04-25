eae0f2598af003e4d06d653e2b7ea3691894303e
Florent VICENTE <vicentegroupe@gmail.com>
2022-04-25 13:36:15 +0200

    checkout steps component

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -22,13 +22,11 @@ export const cartReducer = (state = { cartItems: [], shippingAddress: {} }, acti
       return {
         ...state,
         cartItems: state.cartItems.filter(item => item.product !== action.payload)
-
       }
     case CART_SAVE_SHIPPING_ADDRESS:
       return {
         ...state,
         shippingAddress: action.payload
-
       }
     default:
       return state
