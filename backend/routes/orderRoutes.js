c29d3edf4d04ad2e6e4cb161d91f51e55646f947
Florent VICENTE <vicentegroupe@gmail.com>
2022-04-27 11:03:05 +0200

    update to paid endpoint

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { addOrderItems, getOrderById } from "../controllers/orderController.js"
+import { addOrderItems, getOrderById, updateOrderToPaid } from "../controllers/orderController.js"
 import { protect } from "../middleware/authMiddleware.js"
 
 const router = express.Router()
@@ -8,6 +8,7 @@ const router = express.Router()
 router.route("/").post(protect, addOrderItems)
 // Bien mettre la route /:id APRES la route /
 router.route("/:id").get(protect, getOrderById)
+router.route("/:id/pay").put(protect, updateOrderToPaid)
 
 
 
