7508d6e5268f18c8275e49210d2271098793f6ed
Florent VICENTE <vicentegroupe@gmail.com>
2022-05-08 14:00:55 +0200

    admin product list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ import PlaceOrderScreen from './screens/PlaceOrderScreen'
 import OrderScreen from './screens/OrderScreen'
 import UserListScreen from './screens/UserListScreen'
 import UserEditScreen from './screens/UserEditScreen'
+import ProductListScreen from './screens/ProductListScreen'
 import { Container } from "react-bootstrap"
 import { BrowserRouter as Router, Route } from "react-router-dom"
 
@@ -32,6 +33,7 @@ function App() {
           {/* En mettant un ? derrière le paramètre id, j'indique que le paramètre est facultatif. Si le client veut aller dans son shopping cart vide, il n'y aura pas d id donc le id ne doit pas être obligatoire */}
           <Route path="/cart/:id?" component={CartScreen} />
           <Route path="/admin/userlist" component={UserListScreen} />
+          <Route path="/admin/productlist" component={ProductListScreen} />
           <Route path="/admin/user/:id/edit" component={UserEditScreen} />
           <Route path="/" component={HomeScreen} exact />
         </Container>
