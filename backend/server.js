134f2221e43234edeb981978b7816b96e521160d
Florent VICENTE <vicentegroupe@gmail.com>
2022-05-09 16:41:50 +0200

    image upload with multer

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv"
 import connectDB from "./config/db.js"
@@ -6,6 +7,7 @@ import cors from "cors"
 import productRoutes from "./routes/productRoutes.js"
 import userRoutes from "./routes/userRoutes.js"
 import orderRoutes from "./routes/orderRoutes.js"
+import uploadRoutes from "./routes/uploadRoutes.js"
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
 
 dotenv.config()
@@ -25,8 +27,14 @@ app.get("/", (req, res) => {
 app.use("/api/products", productRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/orders", orderRoutes)
+app.use("/api/upload", uploadRoutes)
 
 app.get("/api/config/paypal", (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
+
+// Pour utiliser __dirname sans la syntaxe "require" modules, on va "mimic" le comportement avec path.resolve()
+// On rend le dossier uploads static pour le rendre accessible par express
+const __dirname = path.resolve()
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
 
 app.use(notFound)
 
