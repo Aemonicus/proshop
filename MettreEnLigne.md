5af5387207385393bfee75718619f968d35e1c62
Florent VICENTE <vicentegroupe@gmail.com>
2022-05-13 09:25:07 +0200

    prepare for heroku deployment

---
+++ MettreEnLigne.md
@@ -0,0 +1,3 @@
+- Aller dans le fichier .env et passer en mode "production"
+
+- Lancer la commande "npm start"
--- backend/server.js
+++ backend/server.js
@@ -17,17 +17,14 @@ connectDB()
 
 const app = express();
 
+const __dirname = path.resolve()
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan("dev"))
 }
 
 app.use(cors())
 app.use(express.json())
-
-
-app.get("/", (req, res) => {
-  res.send("API is running")
-})
 
 app.use("/api/products", productRoutes)
 app.use("/api/users", userRoutes)
@@ -36,9 +33,18 @@ app.use("/api/upload", uploadRoutes)
 
 app.get("/api/config/paypal", (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, "/frontend/build")))
+
+  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "/frontend", "build", "index.html")))
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running")
+  })
+}
+
 // Pour utiliser __dirname sans la syntaxe "require" modules, on va "mimic" le comportement avec path.resolve()
 // On rend le dossier uploads static pour le rendre accessible pour express
-const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.use(notFound)
